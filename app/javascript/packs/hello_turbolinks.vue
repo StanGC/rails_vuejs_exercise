33d0828f58b41f322d1d5902c14e1d308dd4d29a
Stan <solve153@gmail.com>
2017-08-11 13:13:56 +0800

    add vue component

---
+++ app/javascript/packs/hello_turbolinks.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h4>{{ message }}</h4>
+    <ul>
+      <li>Object: {{ obj }} </li>
+      <li>Number: {{ num }} </li>
+      <li>Array: {{ arr }} </li>
+      <li>String: {{ str }} </li>
+    </ul>
+    </div>
+</template>
+
+<script>
+export default {
+  props: ['obj', 'arr', 'num', 'str'],
+  data: function () {
+    return {
+      message: 'Hello, Turbolinks!'
+    }
+  }
+}
+</script>
+
+<style scoped>
+p {
+  font-size: 2em;
+  text-align: center;
+}
+</style>
